351f127fbab4e1edf3ce8a507de1037a9d5276d9
Andrew Lawendy <andrew.lawendy@outlook.com>
2022-02-18 20:47:06 +0200

    feat(design system | input): :sparkles: accept decorators

--- libs/@exercism-ui/input/src/lib/@exercism-ui-input.stories.tsx
+++ libs/@exercism-ui/input/src/lib/@exercism-ui-input.stories.tsx
@@ -1,13 +1,15 @@
 import { Story, Meta } from '@storybook/react';
-import Input from './@exercism-ui-input';
+import Input, { InputProps } from './@exercism-ui-input';
 
 export default {
   title: 'Input',
 } as Meta;
 
-const Template: Story = (args) => <Input {...args} />;
+const Template: Story<InputProps> = (args) => <Input {...args} />;
 
 export const Playground = Template.bind({});
 Playground.args = {
   placeholder: 'Filter by exercise title',
+  preDecorator: '',
+  postDecorator: '',
 };
--- libs/@exercism-ui/input/src/lib/@exercism-ui-input.tsx
+++ libs/@exercism-ui/input/src/lib/@exercism-ui-input.tsx
@@ -2,52 +2,64 @@
 import { forwardRef, useState } from 'react';
 import { useTheme } from '@exercism-testimonials/@exercism-ui/theme';
 
-const Input = forwardRef<
-  HTMLInputElement,
-  React.InputHTMLAttributes<HTMLInputElement>
->(({ onFocus, onBlur, ...props }, ref) => {
-  const [focused, setFocused] = useState(false);
-  const { theme } = useTheme();
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  preDecorator?: React.ReactElement | string;
+  postDecorator?: React.ReactElement | string;
+}
 
-  return (
-    <div
-      sx={{
-        height: 48,
-        backgroundColor: focused ? '#fff' : 'dark-text-label',
-        boxShadow: focused
-          ? `inset 0px 0px 0px 1px ${theme.colors['stroke-light-blue']}, 0px 0px 2px 2px ${theme.colors['active-input-glow']}`
-          : null,
-        borderRadius: 5,
-        px: 22,
-        transition: 'all .2s',
-      }}
-    >
-      <input
-        ref={ref}
-        {...props}
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ preDecorator, postDecorator, onFocus, onBlur, ...props }, ref) => {
+    const [focused, setFocused] = useState(false);
+    const { theme } = useTheme();
+
+    return (
+      <div
         sx={{
-          width: '100%',
-          height: '100%',
-          backgroundColor: 'transparent',
-          borderRadius: 'inherit',
-          border: 0,
-          outline: 0,
+          display: 'flex',
+          alignItems: 'center',
+          height: 48,
+          backgroundColor: focused ? '#fff' : 'dark-text-label',
+          boxShadow: focused
+            ? `inset 0px 0px 0px 1px ${theme.colors['stroke-light-blue']}, 0px 0px 2px 2px ${theme.colors['active-input-glow']}`
+            : null,
+          borderRadius: 5,
+          px: 22,
+          transition: 'all .2s',
+        }}
+      >
+        {preDecorator && <div sx={{ mr: 'spacing-s' }}>{preDecorator}</div>}
 
-          '::placeholder': {
-            color: 'light-label-secondary',
-          },
-        }}
-        onFocus={(e) => {
-          setFocused(true);
-          onFocus?.(e);
-        }}
-        onBlur={(e) => {
-          setFocused(false);
-          onBlur?.(e);
-        }}
-      />
-    </div>
-  );
-});
+        <input
+          ref={ref}
+          {...props}
+          sx={{
+            flexGrow: 1,
+            height: '100%',
+            minWidth: 0,
+            backgroundColor: 'transparent',
+            borderRadius: 'inherit',
+            border: 0,
+            outline: 0,
+
+            '::placeholder': {
+              color: 'light-label-secondary',
+            },
+          }}
+          onFocus={(e) => {
+            setFocused(true);
+            onFocus?.(e);
+          }}
+          onBlur={(e) => {
+            setFocused(false);
+            onBlur?.(e);
+          }}
+        />
+
+        {postDecorator && <div sx={{ ml: 'spacing-s' }}>{postDecorator}</div>}
+      </div>
+    );
+  }
+);
 
 export default Input;
